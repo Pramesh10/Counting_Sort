0dccaf73b0cf286e7132baef1964159ca701ef4d
Pramesh KC <kcpramesh123@hotmail.com>
2024-08-24 21:44:08 +0545

    Add project files.

---
+++ CountingSort.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Counting_Sort
+{
+    public class CountingSort
+    {
+        public static int[] CountingSortFunc(int[] array, int k)
+        {
+           
+            int[] countArray = new int[k];
+
+           
+            for (int i = 0; i < array.Length; i++)
+            {
+                countArray[array[i]]++;
+            }
+                
+
+           
+            for (int i = 1; i < k; i++)
+            {
+                
+                countArray[i] = countArray[i] + countArray[i - 1];
+            }
+                
+
+          
+            int[] resultArray = new int[array.Length];
+
+            
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                resultArray[countArray[array[i]] - 1] = array[i]; 
+                countArray[array[i]] = countArray[array[i]] - 1;
+            }
+
+            return resultArray;
+        }
+
+        public static int[] CountingSortSimple(int[] array)
+        {
+            int[] tempArray = new int[10];
+
+            for (int n = 0; n < array.Length; n++)
+            {
+                tempArray[array[n]]++;
+
+            }
+
+            int resultArrayPossition = 0;
+
+            for (int k = 0; k < tempArray.Length; k++)
+            {
+                for (int i = 0;  i < tempArray[k];  i++)
+                {
+                    array[resultArrayPossition] = k;
+
+                    resultArrayPossition++;
+                }
+
+
+            }
+            return array;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+using Counting_Sort;
+
+int[] exmArray = new int[] { 0, 2, 3, 4, 2, 5, 4, 5, 6, 1, 2, 6, 7, 4, 6, 7, 8, 9, 5, 7, 8, 3, 2, 4, 5, 6, 1 };
+int range = 10;
+int[] result = CountingSort.CountingSortFunc(exmArray, range);
+
+//for(int i=0; i<= result.Length; i++)
+//{
+//    Console.WriteLine(result[i]);
+//}
+
+
+int[] resultSimple = CountingSort.CountingSortSimple(exmArray);
+for (int i = 0; i <= resultSimple.Length; i++)
+{
+    Console.WriteLine(resultSimple[i]);
+}
